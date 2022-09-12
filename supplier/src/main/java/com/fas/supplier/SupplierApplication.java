43875c6a736c457cc0f393379755e0ea50912dc3
Prateeek73 <singh.prateek2599@gmail.com>
2022-09-12 15:40:58 +0530

    bug fix - removed unused import

--- supplier/src/main/java/com/fas/supplier/SupplierApplication.java
+++ supplier/src/main/java/com/fas/supplier/SupplierApplication.java
@@ -2,7 +2,6 @@ package com.fas.supplier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
